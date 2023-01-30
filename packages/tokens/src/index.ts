36659f291ab84e06ed851d985333ab45b9a1c5e1
Igor Trentini <igortrentini.2004@gmail.com>
2023-01-30 15:37:51 -0300

    feat: Criado pacote de tokens, build com TSUP, configuracao de monorepo, typescript e eslint

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+export * from './colors'
+
+export * from './radii'
+export * from './space'
+
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
