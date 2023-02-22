71ba21369b6bf5f48d2b7f9b7420e027164e4a4b
Igor Trentini <igortrentini.2004@gmail.com>
2023-02-22 13:05:58 -0300

    feat(MultiStep): Adicionado componente e sua documentação

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { Check } from "phosphor-react";
+import { ComponentProps, FC } from "react";
+import { CheckboxContainer, CheckboxIndicator } from "./styles";
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export const Checkbox: FC<CheckboxProps> = (props) => {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  );
+};
---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,23 @@
+import { Label, MultiStepContainer, Step, Steps } from "./styles";
+import { FC } from "react";
+
+export interface MultiStepProps {
+  size: number;
+  currentStep?: number;
+}
+
+export const MultiStep: FC<MultiStepProps> = ({ size, currentStep = 1 }) => {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+
+      <Steps css={{ "--steps-size": size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => (
+          <Step key={step} active={currentStep >= step} />
+        ))}
+      </Steps>
+    </MultiStepContainer>
+  );
+};
