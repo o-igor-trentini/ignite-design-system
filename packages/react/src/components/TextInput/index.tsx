a2d49515685ecaa2fa2715319f0baf58262c33d0
Igor Trentini <igortrentini.2004@gmail.com>
2023-02-22 12:45:20 -0300

    feat(TextInput): Adicionado componente e sua documentação

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps, FC } from "react";
+import { Input, Prefix, TextInputContainer } from "./styles";
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string;
+}
+
+export const TextInput: FC<TextInputProps> = ({ prefix, ...props }) => {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+
+      <Input {...props} />
+    </TextInputContainer>
+  );
+};
